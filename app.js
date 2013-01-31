e4f3e2a70d4aef086e704eccb28cfd94c1f6b03f
Justin Durack <justin@magicnotebook.com>
2013-01-30 23:44:12 -0800

    mailReader lib now in use

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-// Running this will kick off the loop that checks for new messages in the email queue
-
 var aws = require ('aws-lib')
   , sqsConnect = require('./lib/sqsConnect')
+  , mailReader = require('./lib/mailReader')
 
 
-sqsConnect.pollMailReaderQueue( function(err, message, callback) {
-  if ( err ) {
-    console.error('Error: app got error from mailReader queue: ', err)
-  } else {
-    console.log('got mailReader message: ', message);
-  }
-  callback();
-});
+sqsConnect.pollMailReaderQueue( mailReader.readMail );
