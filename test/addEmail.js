2a7115475d2ce490a2d9ea9c4363c6dc1a9c329e
Justin Durack <justin@magicnotebook.com>
2013-01-31 17:32:05 -0800

    Added email saving

--- test/addEmail.js
+++ test/addEmail.js
@@ -2,5 +2,8 @@ var sqsConnect = require('../../serverCommon/lib/sqsConnect')
 
 console.log('addEmail app running...');
 
-var message = { 'path' : '/home/jdurack/Documents/emails/emailWith4Attachments.txt' }
+var message = {
+    'path': '/home/jdurack/Documents/emails/emailWith4Attachments.txt'
+  , 'userId': 'asdf'
+}
 sqsConnect.addMessageToMailReaderQueue( message );
