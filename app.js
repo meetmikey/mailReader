a2ae578f033de565bca2d4bbd80a9e0b90b177f1
Justin Durack <justin@magicnotebook.com>
2013-02-01 19:31:42 -0800

    basic attachment uploading working

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var serverCommon = process.env.SERVER_COMMON;
 
-var conf = require(serverCommon + '/conf')
-  , sqsConnect = require('../serverCommon/lib/sqsConnect')
+var sqsConnect = require('../serverCommon/lib/sqsConnect')
   , mailReader = require('./lib/mailReader')
+
 
 console.log('mailReader app running...');
 
