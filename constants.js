4035ec06f42c7e7a7b982e98933f874f0d91a6e3
Justin Durack <justin@magicnotebook.com>
2013-02-15 11:53:55 -0800

    Re-worked links, linkInfos, promotability.

---
+++ constants.js
@@ -0,0 +1,22 @@
+
+function define(name, value) {
+  Object.defineProperty(exports, name, {
+    value : value,
+    enumerable: true
+  });
+}
+
+var urlFilterText = [
+    'track'
+  , 'unsub'
+  , 'sendgrid.me'
+  , 'api.mixpanel.com'
+  , 'eventbrite.com'
+  , 'evite.com'
+  , 'www.w3.org'
+  , 'mailchimp.com'
+];
+
+define('URL_FILTER_TEXT', urlFilterText);
+define('PDF_DOWNLOAD_TIMEOUT', 20000);
+define('LINK_TEXT_CUTOFF', 300);
