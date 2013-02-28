e99dd289b638c03b8c4147e4a48071cf0f618735
Justin Durack <justin@magicnotebook.com>
2013-02-27 16:20:48 -0800

    Only store the mail body in local environments.

--- conf.js
+++ conf.js
@@ -1,6 +1,19 @@
+var environment = process.env.NODE_ENV;
+
+//Local
+var storeMailBody = true;
+
+if (environment == 'production') {
+  storeMailBody = false;  
+
+} else if (environment == 'development') {
+  storeMailBody = false;
+}
+
 module.exports = {
     diffbot : {
       token : 'b45dc70b4a560b2b106a136212486c0e'
   }
   , googleDriveAPIFileGetPrefix: 'https://www.googleapis.com/drive/v2/files/'
+  , storeMailBody: storeMailBody
 }
