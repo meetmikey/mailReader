4976b5d9858b55bb5ce2fdd765ac1fe934c916ab
Justin Durack <justin@magicnotebook.com>
2013-03-15 12:42:36 -0700

    new app init method

--- mailReader.js
+++ mailReader.js
@@ -1,50 +1,49 @@
 var serverCommon = process.env.SERVER_COMMON;
 
-var winston = require (serverCommon + '/lib/winstonWrapper').winston;
-winston.logBreak();
-winston.doInfo('mailReader app running...');
-
-var mongoose = require(serverCommon + '/lib/mongooseConnect')
+var appInitUtils = require(serverCommon + '/lib/appInitUtils')
+  , winston = require(serverCommon + '/lib/winstonWrapper').wrapper
   , sqsConnect = require(serverCommon + '/lib/sqsConnect')
   , mailReader = require('./lib/mailReader')
   , mailReaderConstants = require('./constants')
 
+var initActions = [
+    appInitUtils.CONNECT_ELASTIC_SEARCH
+  , appInitUtils.CONNECT_MONGO
+];
 
-process.on('uncaughtException', function (err) {
-  winston.doError('uncaughtException:', {stack : err.stack, message : err.message});
-  process.exit(1);
+//initApp() will not callback an error.
+//If something fails, it will just exit the process.
+appInitUtils.initApp( 'mailReader', initActions, function() {
+  var maxHandlers = mailReaderConstants.MAX_HANDLERS;
+  if ( process && process.argv && ( process.argv.length > 2 ) ) {
+    maxHandlers = process.argv[2];
+  }
+
+  winston.doInfo('maxHandlers: ' + maxHandlers);
+
+  sqsConnect.pollMailReaderQueue(
+    function(messageString, callback) {
+      mailReader.handleMailMessage( messageString, function(err) {
+        if (err) {
+          winston.handleError(err);
+          callback(err);
+        } else {
+          callback();
+        }
+      })
+    }, maxHandlers
+  );
+
+  sqsConnect.pollMailReaderQuickQueue(
+    function(messageString, callback) {
+      mailReader.handleMailMessage( messageString, function(err) {
+        if (err) {
+          winston.handleError(err);
+          callback(err);
+        } else {
+          callback();
+        }
+      })
+    }, maxHandlers
+  );
 });
-
-var maxHandlers = mailReaderConstants.MAX_HANDLERS;
-if ( process && process.argv && ( process.argv.length > 2 ) ) {
-  maxHandlers = process.argv[2];
-}
-
-winston.doInfo('maxHandlers: ' + maxHandlers);
-
-sqsConnect.pollMailReaderQueue(
-  function(messageString, callback) {
-    mailReader.handleMailMessage( messageString, function(err) {
-      if (err) {
-        winston.handleError(err);
-        callback(err);
-      } else {
-        callback();
-      }
-    })
-  }, maxHandlers
-);
-
-
-sqsConnect.pollMailReaderQuickQueue(
-  function(messageString, callback) {
-    mailReader.handleMailMessage( messageString, function(err) {
-      if (err) {
-        winston.handleError(err);
-        callback(err);
-      } else {
-        callback();
-      }
-    })
-  }, maxHandlers
-);
