0813dc2ab32fe4679981d17dbacb9668303cf19a
Justin Durack <justin@magicnotebook.com>
2013-04-12 17:01:23 -0700

    upgraded to match new serverCommon followRedirects branch

--- test/httpsImageIntoAzure.js
+++ test/httpsImageIntoAzure.js
@@ -1,10 +1,12 @@
 var serverCommon = process.env.SERVER_COMMON;
-var azureUtils = require (serverCommon + '/lib/azureUtils')
+
+var cloudStorageUtils = require (serverCommon + '/lib/cloudStorageUtils')
     , http = require ('http')
     , conf = require (serverCommon + '/conf');
 
 
-azureUtils.downloadAndSaveStaticImage('http://1800hocking.files.wordpress.com/2011/07/hi-ohio-logo.jpg', function (err, path) {
+var url = 'http://1800hocking.files.wordpress.com/2011/07/hi-ohio-logo.jpg';
+cloudStorageUtils.downloadAndSaveStaticImage( url, true, function (err, path) {
   if (err) {
     console.error ('test failed', err);
     return;
--- test/httpsImageIntoS3.js
+++ test/httpsImageIntoS3.js
@@ -1,6 +1,6 @@
 var serverCommon = process.env.SERVER_COMMON;
 
-var s3Utils = require(serverCommon + '/lib/s3Utils')
+var cloudStorageUtils = require(serverCommon + '/lib/cloudStorageUtils')
   , conf = require(serverCommon + '/conf')
   , winston = require(serverCommon + '/lib/winstonWrapper').winston
 
@@ -9,7 +9,7 @@ var url = 'https://www.meetmikey.com/domains/www.meetmikey.com/web/content/img/m
 //var url = 'http://topwalls.net/wallpapers/2012/10/Hulk-Green-2048x2048.jpg';
 //var s3Path = conf.aws.s3Folders.static + '/TEMP_TEST';
 
-s3Utils.downloadAndSaveStaticImage(url, function(err) {
+cloudStorageUtils.downloadAndSaveStaticImage( url, false, function(err) {
   if ( err ) {
     winston.handleError( err );
   } else {
