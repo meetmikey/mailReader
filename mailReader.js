0ea42c7cbb420a8948d12d00351aae67543abc13
Justin Durack <justin@magicnotebook.com>
2013-04-25 15:38:35 -0700

    turned off memwatch

--- mailReader.js
+++ mailReader.js
@@ -10,7 +10,7 @@ var appInitUtils = require(serverCommon + '/lib/appInitUtils')
 var initActions = [
     appInitUtils.CONNECT_ELASTIC_SEARCH
   , appInitUtils.CONNECT_MONGO
-  , appInitUtils.MEMWATCH_MONITOR
+  //, appInitUtils.MEMWATCH_MONITOR
 ];
 
 //initApp() will not callback an error.
