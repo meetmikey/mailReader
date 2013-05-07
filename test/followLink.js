2c367fe1849598a6aefa45ba9c3dbfef47f5040f
Sagar Mehta <mehta.sagarv@gmail.com>
2013-05-06 19:08:29 -0700

    updates to linkHandler, followLink to not deal with nonpromotion

--- test/followLink.js
+++ test/followLink.js
@@ -10,9 +10,9 @@ var linkInfo = new LinkInfoModel();
 
 var linkInfo = {
   "_id" : "517dc7754e03984f34b6a32a",
-  "comparableURL" : "fragomen.com/emailmarketing/clickthroughhandler.aspx?link=%3ca+href%3d%22http%3a%2f%2fwww.cbp.gov%2fi94%22%3ehttp%3a%2f%2fwww.cbp.gov%2fi94%3c%2fa%3e&nid=f0c10c7e-214b-4446-9978-7cbe8be871bd&clid=5dc90fc9-8ae6-4e70-9dea-d99821dd2cc4&cid=2b2a8303-71ea-4e1a-b58a-035b95b01c20&ce=jf6hyhuzmbj2oefi7j9mbrka16mikjhvcdmmlinbzdm%3d",
+  "comparableURL" : "angel.co/jetlore",
   "comparableURLHash" : "556e0991b005320cf5bc02851fafced4f258b7209bb4fefb3855a3a52fe3f711",
-  "rawURL" : "http://www.fragomen.com/EmailMarketing/ClickThroughHandler.aspx?link=%3ca+href%3d%22http%3a%2f%2fwww.cbp.gov%2fI94%22%3ehttp%3a%2f%2fwww.cbp.gov%2fI94%3c%2fa%3e&amp;nid=f0c10c7e-214b-4446-9978-7cbe8be871bd&amp;clid=5dc90fc9-8ae6-4e70-9dea-d99821dd2cc4&amp;cid=2b2a8303-71ea-4e1a-b58a-035b95b01c20&amp;ce=JF6hyhUzMBj2OEFi7j9Mbrka16MiKjHvcdmMLInBzDM%3d"
+  "rawURL" : "https://angel.co/jetlore"
 }
 
 
